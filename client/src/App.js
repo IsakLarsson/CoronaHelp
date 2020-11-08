d9066f50b5bebbdbd99057dbc63702ee3698c59d
Viktor Malmedal <viktormalmedal@gmail.com>
2020-11-08 13:42:05 +0100

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./index.scss";
+import firebase from "./firebase/Firebase";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hello world</h1>
+    </div>
+  );
+}
+
+export default App;
